18f75a5005ec14ae3f46b8749f9add5447cfef21
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2017-11-08 23:37:55 -0600

    Implement form

--- src/app/page/enroll-form/enroll-form.component.ts
+++ src/app/page/enroll-form/enroll-form.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormArray } from '@angular/forms';
+import { User } from 'models';
 
 @Component({
   selector: 'enroll-form',
   templateUrl: './enroll-form.component.html',
   styleUrls: ['./enroll-form.component.scss']
 })
-export class EnrollFormComponent { }
+export class EnrollFormComponent {
+  @Input()
+  parentForm: FormGroup;
+
+  @Output()
+  removed = new EventEmitter<any>();
+
+  get applicants() {
+    return (this.parentForm.get('applicants') as FormArray)
+      .controls;
+  }
+
+  removeForm(applicant: User, index: number): void {
+    this.removed.emit({ applicant, index });
+  }
+}
--- src/app/page/landing/landing.component.ts
+++ src/app/page/landing/landing.component.ts
@@ -1,8 +1,47 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormArray, FormControl } from '@angular/forms';
+import { User } from 'models';
 
 @Component({
   selector: 'landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
-export class LandingComponent { }
+export class LandingComponent {
+  form: FormGroup;
+
+  get applicants() {
+    return (this.form.get('applicants') as FormArray)
+      .controls;
+  }
+
+  constructor() {
+    this.form = new FormGroup({
+      applicants: new FormArray([])
+    });
+  }
+
+  addNewForm(): void {
+    (this.form.get('applicants') as FormArray)
+      .push(this.createFormGroup());
+  }
+
+  onSubmit(): void {
+    console.log('Submit: ', this.form.value);
+  }
+
+  onRemove({ applicant, index }: { applicant: User, index: number }): void {
+    const control = this.form.get('applicants') as FormArray;
+
+    control.removeAt(index);
+  }
+
+  private createFormGroup(): FormGroup {
+    return new FormGroup({
+      firstName: new FormControl(''),
+      lastName: new FormControl(''),
+      occupation: new FormControl(''),
+      addressLine: new FormControl('')
+    });
+  }
+}
